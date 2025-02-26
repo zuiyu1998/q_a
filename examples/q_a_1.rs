df487df7ef52734a238fc467f8217fed644f2aad
zuiyu1998 <1542844298@qq.com>
2025-02-26 10:10:48 +0800

    feat: q_a_1

---
+++ examples/q_a_1.rs
@@ -0,0 +1,48 @@
+use std::{
+    sync::{Arc, Mutex},
+    thread::{self, sleep},
+    time::Duration,
+};
+
+#[derive(Debug, Default, Clone)]
+pub struct State {
+    count: Arc<Mutex<usize>>,
+}
+
+impl State {
+    pub fn computed(&self) {
+        println!("computed");
+        let mut count = self.count.lock().unwrap();
+
+        sleep(Duration::from_secs(2));
+
+        *count += 1;
+    }
+
+    pub fn print(&self) {
+        let count = self.count.lock().unwrap();
+
+        println!("{}", count);
+    }
+}
+
+fn main() {
+    let state = State::default();
+
+    let state_clone = state.clone();
+    thread::spawn(move || {
+        state_clone.computed();
+    });
+
+
+    let state_clone = state.clone();
+    thread::spawn(move || {
+        state_clone.computed();
+    });
+
+    for _ in 0..1000 {
+        state.print();
+    }
+
+    sleep(Duration::from_secs(3));
+}
